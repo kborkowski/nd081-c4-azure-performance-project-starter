13b871fef425ddf928851fda5c19580b90e3f3ec
Krzysztof Borkowski <v-krbork@microsoft.com>
2025-11-24 16:50:14 +0100

    Fix: Change app to run on port 5000 with debug=False for production

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -101,5 +101,5 @@ def index():
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
 if __name__ == "__main__":
-    # For local development only
-    app.run(host='0.0.0.0', port=80, debug=True)
+    # For production use
+    app.run(host='0.0.0.0', port=5000, debug=False)
